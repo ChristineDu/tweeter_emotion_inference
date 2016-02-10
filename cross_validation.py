ea638a5bc63cffeb1680be0b988eeaaff05c45ae
Christine Du <duyuqingecho@gmail.com>
2016-02-09 22:05:27 -0500

    finished WEKA

--- cross_validation.py
+++ cross_validation.py
@@ -1,38 +1,28 @@
+from scipy import stats
+
 def divide_files():
 	file_names = ["part1.arff", "part2.arff", "part3.arff", "part4.arff", "part5.arff", \
 				"part6.arff", "part7.arff", "part8.arff", "part9.arff", "part10.arff",]
 	fin = open("all.arff",'r')
 	# cope with non-data lines in the begining
 	beginlines = []
-	k = 0
-	while k < 25:
+	for k in range(0,25):
 		beginlines.append(fin.readline())
-		k += 1
 	# copy 550 data from class 0 to each file
-	i = 0
-	while i<10:
+	for i in (0,10):
 		fout = open(file_names[i],'a')
-		k = 0
-		while k < 25:
+		for k in range(0,25):
 			fout.write(beginlines[k])
-			k += 1
-		j = 0
-		while j < 550:
+		for j in range(0,550):
 			line = fin.readline()
 			fout.write(line)
-			j += 1
-		i += 1
 		fout.close()
 	# copy 550 data from class 4 to each file
-	i = 0
-	while i<10:
+	for i in (0,10):
 		fout = open(file_names[i],'a')
-		j = 0
-		while j < 550:
+		for j in range(0,550):
 			line = fin.readline()
 			fout.write(line)
-			j += 1
-		i += 1
 		fout.close()
 	fin.close()
 
@@ -44,46 +34,86 @@ def combine_files():
 	# cope with non-data lines in the begining
 	beginlines = []
 	fin = open(file_names[0],'r')
-	k = 0
-	while k < 25:
+	for k in range(0,25):
 		beginlines.append(fin.readline())
-		k += 1
 	fin.close()
 
 	# for each output file
-	i = 0
-	while i < 10:
+	for i in range(0,10):
 		fout = open(file_names1[i],'a')
 		# write begining lines
-		k = 0
-		while k < 25:
+		for k in range(0,25):
 			fout.write(beginlines[k])
-			k += 1
 		# open readfiles one by one
-		j = 0
-		while j < 10:
+		for j in range(0,10):
 			if j == i: 
 				j += 1
 				continue
 			fin = open(file_names[j])
-			k = 0
-			while k < 25:
+			for k in range(0,25):
 				fin.readline()
-				k += 1
 			line = fin.readline()
 			while line != "":
 				fout.write(line)
 				line = fin.readline()
 				#print line
 			print file_names[j]
-			j+=1
 			fin.close()
 		fout.close()
 		print file_names1[i]
-		i += 1
+
+def calculate():
+	fin = open("record1.txt",'r')
+	fout = open("output3.4.txt",'a')
+	for x in range(0,30):
+		line = fin.readline()
+		a = [int(n) for n in line.split(" ")]
+		accuracy = (a[0] + a[3]) * 1.0 / (a[0] + a[1] + a[2] +a[3])
+		precision1 = a[0] * 1.0 / (a[0] + a[2])
+		precision2 = a[3] * 1.0 / (a[1] + a[3])
+		recall1 = a[0] * 1.0 / (a[0] + a[1])
+		recall2 = a[3] * 1.0 / (a[2] + a[3])
+		b = str(accuracy)+"\t"+str(precision1)+"\t"+str(precision2)+"\t"+str(recall1)+"\t"+str(recall2) +"\n"
+		fout.write(b)
+	fin.close()
+	fout.close()
+
+def acurracy_comp():
+	fin = open("output3.4.txt",'r')
+	nb = []
+	svm = []
+	dt = []
+	for x in range(0,10):
+		line = fin.readline()
+		a = [float(n) for n in line.split("\t")]
+		svm.append(a[0])
+	for y in range(0,10):
+		line = fin.readline()
+		a = [float(n) for n in line.split("\t")]
+		nb.append(a[0])
+	for z in range(0,10):
+		line = fin.readline()
+		a = [float(n) for n in line.split("\t")]
+		dt.append(a[0])
+	svm_nb = stats.ttest_rel(svm, nb)
+	nb_dt = stats.ttest_rel(nb, dt)
+	svm_dt = stats.ttest_rel(svm, dt)
+	print svm_nb
+	print nb_dt
+	print svm_dt
+
+
+
+
+
+
+
+
 
 
 
 if __name__ == '__main__':
-    divide_files()
-    combine_files()
+    #divide_files()
+    #combine_files()
+    calculate()
+    acurracy_comp()
